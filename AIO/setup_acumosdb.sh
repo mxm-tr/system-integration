ebf9a25381fe15ca5aca07fdfaf20e025bc9384b
mwelss <martin.welss@iais.fraunhofer.de>
2020-10-06 10:06:59 -0700

    fix path to db-init script

--- AIO/setup_acumosdb.sh
+++ AIO/setup_acumosdb.sh
@@ -69,7 +69,7 @@ function new_db() {
   # NOTE: Naming convention change in sql scripts as of 3.0-rev2 !
   # See https://github.com/acumos/common-dataservice/tree/master/cmn-data-svc-server/db-scripts
   if [[ $(ls cmn-data-svc-ddl-dml-*) != "" ]]; then rm cmn-data-svc-ddl-dml-*; fi
-  wget https://raw.githubusercontent.com/acumos/common-dataservice/master/cmn-data-svc-server/db-scripts/cmn-data-svc-ddl-dml-$ACUMOS_CDS_VERSION.sql
+  wget https://raw.githubusercontent.com/acumos/common-dataservice/master/cmn-data-svc/cmn-data-svc-server/db-scripts/cmn-data-svc-ddl-dml-$ACUMOS_CDS_VERSION.sql
   sedi "1s/^/use $ACUMOS_CDS_DB;\n/" cmn-data-svc-ddl-dml-$ACUMOS_CDS_VERSION.sql
   mysql $server --user=$ACUMOS_MARIADB_USER --password=$ACUMOS_MARIADB_USER_PASSWORD < cmn-data-svc-ddl-dml-$ACUMOS_CDS_VERSION.sql
 }
